e32f790f01f43338755ec236d3365313a7b9b372
Matt Mueller <mattmuelle@gmail.com>
2012-12-23 23:41:02 -0600

    ready to fill in

--- index.js
+++ index.js
@@ -1,2 +1,72 @@
+/**
+ * Export `Monk`
+ */
 
-module.exports = require('./lib/model');
+module.exports = function(url) {
+  var monk = require('monk')(url);
+
+  return function(model) {
+    var db = monk.get(model.modelName);
+    var sync = new Sync(db);
+    model.sync = sync;
+
+    // Possibly go with the mixin
+    model.index = db.index.bind(db);
+  };
+};
+
+/**
+ * Initialize `Sync`
+ */
+
+function Sync(db) {
+  this.db = db;
+}
+
+/**
+ * All
+ */
+
+Sync.prototype.all = function(query, fn) {
+
+};
+
+/**
+ * Get
+ */
+
+Sync.prototype.get = function(query, fn) {
+
+};
+
+/**
+ * removeAll
+ */
+
+Sync.prototype.removeAll = function(query, fn) {
+
+};
+
+/**
+ * save
+ */
+
+Sync.prototype.save = function(query, fn) {
+
+};
+
+/**
+ * update
+ */
+
+Sync.prototype.update = function(query, fn) {
+
+};
+
+/**
+ * remove
+ */
+
+Sync.prototype.remove = function(query, fn) {
+
+};
--- test/test.js
+++ test/test.js
@@ -1,39 +1,9 @@
-var model = require('../').connect('localhost/monk-model'),
-    pwd = require('pwd');
-    User = model('user');
+var model = require('node-model');
+    mongo = require('../')('localhost/test');
 
-User.attr('_id')
-    .attr('name')
-    .attr('email')
-    .attr('password')
-    .attr('plan');
+var User = model('user')
+  .attr('name')
+  .attr('email')
+  .attr('password');
 
-User.use(hash);
-
-var user = new User();
-
-user.name('jimmy')
-    .email('mattmuelle@gmail.com')
-    .password('lol')
-    .plan('free');
-
-// User.all(function(err, users) {
-//   users[1].name('martha');
-//   // console.log(users[1].toJSON());
-// });
-
-user.save(function(err) {
-  console.log(user);
-});
-
-
-function hash(model) {
-  model.attr('salt', { required : true });
-  model.on('saving', function(obj, done) {
-    pwd.hash(obj.password(), function(err, hash) {
-      if(err) obj.errors.push(err);
-      obj.salt(hash);
-      done();
-    });
-  });
-}
+User.use(mongo);
