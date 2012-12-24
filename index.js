3a77f7cbbc5545f1e23e1891f46dcb0356e8b4ea
Matt Mueller <mattmuelle@gmail.com>
2012-12-23 23:59:48 -0600

    filled in

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /**
+ * Module dependencies
+ */
+
+var sync = {};
+
+/**
  * Export `Monk`
  */
 
@@ -7,8 +13,8 @@ module.exports = function(url) {
 
   return function(model) {
     var db = monk.get(model.modelName);
-    var sync = new Sync(db);
     model.sync = sync;
+    model.db = db;
 
     // Possibly go with the mixin
     model.index = db.index.bind(db);
@@ -16,57 +22,85 @@ module.exports = function(url) {
 };
 
 /**
- * Initialize `Sync`
- */
-
-function Sync(db) {
-  this.db = db;
-}
-
-/**
  * All
  */
 
-Sync.prototype.all = function(query, fn) {
+sync.all = function(query, fn) {
+  if (arguments.length == 1) {
+    fn = query;
+    query = {};
+  }
 
+  this.db.find(query, function(err, models) {
+    if (err) return fn(err);
+    else if (!models) return fn(null, false);
+    return fn(null, models);
+  });
 };
 
 /**
  * Get
  */
 
-Sync.prototype.get = function(query, fn) {
+sync.get = function(query, fn) {
+  var action = 'findOne';
 
+  if ('string' == typeof query) action = 'findById';
+
+  this.db[action](query, function(err, model) {
+    if(err) return fn(err);
+    else if(!model) return fn(null, false);
+    return fn(null, model);
+  });
 };
 
 /**
  * removeAll
  */
 
-Sync.prototype.removeAll = function(query, fn) {
-
+sync.removeAll = function(query, fn) {
+  this.db.remove(query, fn);
 };
 
 /**
  * save
  */
 
-Sync.prototype.save = function(query, fn) {
-
+sync.save = function(fn) {
+  this.model.db.insert(this.attrs, function(err, doc) {
+    if(err) return fn(err);
+    return fn(err, doc);
+  });
 };
 
 /**
  * update
  */
 
-Sync.prototype.update = function(query, fn) {
+sync.update = function(fn) {
+  var db = this.model.db,
+      id = this.primary(),
+      changed = this.changed();
 
+  db.findAndModify({ _id : id }, { $set : changed }, function(err) {
+    return fn(err);
+  });
 };
 
 /**
  * remove
  */
 
-Sync.prototype.remove = function(query, fn) {
+sync.remove = function(query, fn) {
+  var db = this.model.db,
+      id = this.primary();
 
+  if (arguments.length == 1) {
+    fn = query;
+    query = { _id : id };
+  }
+
+  db.remove(query, function(err) {
+    return fn(err);
+  });
 };
