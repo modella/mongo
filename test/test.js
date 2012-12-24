a753df73ed5f96b6c17a3ba9f7c7968a82cce2b4
Matt Mueller <mattmuelle@gmail.com>
2012-12-24 00:08:58 -0600

    fixed test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,24 @@
 var model = require('node-model');
-    mongo = require('../')('localhost/test');
+    mongo = require('../')('localhost/monk-model');
 
 var User = model('user')
+  .attr('_id')
   .attr('name')
   .attr('email')
   .attr('password');
 
 User.use(mongo);
+
+/**
+ * Initialize
+ */
+
+var user = new User;
+
+user.name('matt')
+    .email('mattmuelle@gmail.com')
+    .password('test');
+
+user.save(function(err) {
+  console.log(user);
+});
